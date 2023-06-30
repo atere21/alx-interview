5c436c14b9b03352d42d9c8692cbec4b6e547c75
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-06-30 09:06:08 -0700

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,21 +3,21 @@
 
 
 def pascal_triangle(n):
-        '''
-            Pascal's triangle
-                Args:
-                      n (int): The number of rows of the triangle
-                          Returns:
-                                List of lists of integers representing the Pascal’s triangle
-                                    '''
-                                        lists = []
-                                            if n == 0:
-                                                        return lists
-                                                        for i in range(n):
-                                                                    lists.append([])
-                                                                            lists[i].append(1)
-                                                                                    if (i > 0):
-                                                                                                    for j in range(1, i):
-                                                                                                                        lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
-                                                                                                                                    lists[i].append(1)
-                                                                                                                                        return lists
+    '''
+    Pascal's triangle
+    Args:
+      n (int): The number of rows of the triangle
+    Returns:
+      List of lists of integers representing the Pascal’s triangle
+    '''
+    lists = []
+    if n == 0:
+        return lists
+    for i in range(n):
+        lists.append([])
+        lists[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
+            lists[i].append(1)
+    return lists
