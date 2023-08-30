6885698b09124a0186c203bc2fa5edcd60e16bb1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-08-29 23:24:43 -0700

    Update 0-island_perimeter.py

--- 0x09-island_perimeter /0-island_perimeter.py
+++ 0x09-island_perimeter /0-island_perimeter.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""
-Island Perimeter:
-    returns the perimeter of the island described in grid
+"""Island perimeter computing module.
 """
 
 
 def island_perimeter(grid):
-    """island perimenter function"""
+    """Computes the perimeter of an island with no lakes.
+    """
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
